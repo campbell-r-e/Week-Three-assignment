540d93c066e6ef41bfa348c7bc330c071837f885
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-09 22:46:07 -0400

    intials

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+function card1(){
+    document.getElementById("value1").innerText = 0;
+};
+function card2(){
+    document.getElementById("value2").innerText = 1;
+}
+function card3(){
+    document.getElementById("value2").innerText = 1;
+}
+function card4(){
+    document.getElementById("value2").innerText = 0;
+}
+function card5(){
+    document.getElementById("value2").innerText = 2;
+}
+function card6(){
+    document.getElementById("value2").innerText = 2;
+}
+function card7(){
+    document.getElementById("value2").innerText = 3;
+}
+function card8(){
+    document.getElementById("value2").innerText = 3;
+}
+
+
+
