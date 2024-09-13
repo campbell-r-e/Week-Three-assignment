3b4d58a68e6d18d60e333e19aa406b424d52db31
bsucs <122378640+bsucs@users.noreply.github.com>
2024-09-12 21:19:19 -0400

    final

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var card4_val = 0;
 var card5_val = 0;
 var card6_val = 0;
 var card7_val = 0;
+var matches = 4;
+
+document.getElementById("Score").innerText = 4;
 
 document.getElementById("w1").addEventListener("click", card0);
 document.getElementById("w2").addEventListener("click", card1);
@@ -65,13 +68,30 @@ function card7() {
 }
 
 function check() {
+
     
-    if (card3_val === card0_val && card0_val !== 0 && card3_val !== 0) {
+
+    
+   
+    
+    if(card3_val === card0_val && card0_val !== 0 && card3_val !== 0) {
         setTimeout(() => {
             remove_card3_card0();
         }, 1000);
-        return; 
+        matches=matches-1;
+        document.getElementById("Score").innerText = matches;
+        //return; 
         // 17's
+    }else{
+        setTimeout(() => {
+            document.getElementById("w4").innerText = "";
+            document.getElementById("w0").innerText = "";
+            card3_val=0;
+            card0_val =0;
+        }, 1000);
+        
+        // 17's
+
     }
 
     
@@ -79,29 +99,55 @@ function check() {
         setTimeout(() => {
             remove_card_1_card2();
         }, 1000);
-        return; 
+        //return; 
 
         // 1's
-    }
+        matches=matches-1;
+        document.getElementById("Score").innerText = matches;
+    }else{
+        setTimeout(() => {
+            document.getElementById("w2").innerText = "";
+            document.getElementById("w3").innerText = "";
+            card2_val=0;
+            card1_val =0;
+        }, 1000);
 
     if (card4_val === card5_val && card5_val !== 0 && card4_val !== 0) {
         setTimeout(() => {
             remove_card_4_card5();
         }, 1000);
-        return; 
+        //return; 
         // 2's
-    }
+        matches=matches-1;
+        document.getElementById("Score").innerText = matches;
+    }else{
+        setTimeout(() => {
+            document.getElementById("w5").innerText = "";
+            document.getElementById("w6").innerText = "";
+            card4_val=0;
+            card5_val =0;
+        }, 1000);
 
 
     if (card6_val === card7_val && card6_val !== 0 && card7_val !== 0) {
         setTimeout(() => {
             remove_card_7_card6();
         }, 1000);
-        return; 
+        //return; 
         // 3's
+        matches=matches-1;
+        document.getElementById("Score").innerText = matches;
+    }else{
+        setTimeout(() => {
+            document.getElementById("w7").innerText = "";
+            document.getElementById("w8").innerText = "";
+            card6_val=0;
+            card7_val =0;
+        }, 1000);}
+    
+    
+}
     }
-    
-    
 }
 
 function remove_card3_card0() {
@@ -120,7 +166,7 @@ function remove_card3_card0() {
 
     
     card0_val = 0;
-    card3_val = 0;
+    card1_val = 0;
 }
 
 function remove_card_1_card2() {
@@ -138,16 +184,51 @@ function remove_card_1_card2() {
     };
 
     
-    card1_val = 0;
     card2_val = 0;
+    card3_val = 0;
 }
 
-
+// new 
 function remove_card_4_card5(){
+
+
+    var w5 = document.getElementById("w5");
+    var w6 = document.getElementById("w6");
+    
+    if (w5!=null) {
+        w5.remove()
+
+    };
+    if (w6!= null) {
+        w6.remove()
+
+    };
+
+    
+    card4_val = 0;
+    card5_val = 0;
 
 }
 
 function remove_card_7_card6(){
+
+
+
+    var w7 = document.getElementById("w7");
+    var w8 = document.getElementById("w8");
     
+    if (w7!=null) {
+        w7.remove()
+
+    };
+    if (w8!= null) {
+        w8.remove()
+
+    };
+
+    
+    card6_val = 0;
+    card7_val = 0;
+
 }
 
